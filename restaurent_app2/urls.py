bc5f3bb5d2a1b5a569d2d44099c764d1ef694729
Nikhila Ajayan <nikhilaajayan76@gmail.com>
2023-06-12 21:44:29 +0530

    second_commit

--- restaurent_app2/urls.py
+++ restaurent_app2/urls.py
@@ -3,5 +3,16 @@ from restaurent_app2 import views
 
 urlpatterns=[
     path('',views.home_page,name='home_page'),
+    path('aboutus',views.aboutus,name='aboutus'),
+    path('contactus',views.contactus,name='contactus'),
+    path('menu',views.menu,name='menu'),
+    path('blog',views.blog,name='blog'),
+    path('blog_detail',views.blog_detail,name='blog_detail'),
+    path('menu_detail',views.menu_detail,name='menu_detail'),
+    path('login',views.login,name='login'),
+
+
+
+
     
 ]
--- restaurent_app2/views.py
+++ restaurent_app2/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render,redirect
 # Create your views here.
 def home_page(request):
     return render(request,'home.html')
+
+def aboutus(request):
+    return render(request,'about.html')
+
+def contactus(request):
+    return render(request,'contact.html')
+
+def menu(request):
+    return render(request,'menu.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def blog_detail(request):
+    return render(request,'blog_detail.html')
+
+def menu_detail(request):
+    return render(request,'menu_detail.html')
+
+def login(request):
+    return render(request,'login.html')
