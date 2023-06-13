9fd5d7521399e5c3a9c2e74c5c40cbcf5d45a6fb
Nikhila Ajayan <nikhilaajayan76@gmail.com>
2023-06-13 22:16:50 +0530

    third_commit

--- restaurent_app2/urls.py
+++ restaurent_app2/urls.py
@@ -4,12 +4,19 @@ from restaurent_app2 import views
 urlpatterns=[
     path('',views.home_page,name='home_page'),
     path('aboutus',views.aboutus,name='aboutus'),
-    path('contactus',views.contactus,name='contactus'),
-    path('menu',views.menu,name='menu'),
+    path('contactus',views.contactus,name='contactus'),    
+    path('contactus1',views.contactus1,name='contactus1'),
+    path('menu',views.menu,name='menu'),    
+    path('menu1',views.menu1,name='menu1'),
     path('blog',views.blog,name='blog'),
     path('blog_detail',views.blog_detail,name='blog_detail'),
     path('menu_detail',views.menu_detail,name='menu_detail'),
     path('login',views.login,name='login'),
+    path('cart',views.cart,name='cart'),
+    path('home_page1',views.home_page1,name='home_page1'),    
+    path('aboutus1',views.aboutus1,name='aboutus1'),
+
+
 
 
 
--- restaurent_app2/views.py
+++ restaurent_app2/views.py
@@ -5,17 +5,32 @@ from django.shortcuts import render,redirect
 def home_page(request):
     return render(request,'home.html')
 
+def home_page1(request):
+    return render(request,'home1.html')
+
 def aboutus(request):
     return render(request,'about.html')
+
+def aboutus1(request):
+    return render(request,'about1.html')
 
 def contactus(request):
     return render(request,'contact.html')
 
+def contactus1(request):
+    return render(request,'contact1.html')
+
 def menu(request):
     return render(request,'menu.html')
 
+def menu1(request):
+    return render(request,'menu1.html')
+
 def blog(request):
     return render(request,'blog.html')
+
+def blog1(request):
+    return render(request,'blog1.html')
 
 def blog_detail(request):
     return render(request,'blog_detail.html')
@@ -23,5 +38,11 @@ def blog_detail(request):
 def menu_detail(request):
     return render(request,'menu_detail.html')
 
+def menu_detail1(request):
+    return render(request,'menu_detail1.html')
+
 def login(request):
     return render(request,'login.html')
+
+def cart(request):
+    return render(request,'cart.html')
